86fa288c7a2abf5633b6527558a7309b87a149ae
Sabya <sabya.gupta@yahoo.com>
2019-04-29 21:40:31 +0530

    Config

---
+++ src/main/java/my/spring/sec/config/AppConfig.java
@@ -0,0 +1,12 @@
+package my.spring.sec.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="my.spring.sec")
+public class AppConfig {
+
+}
